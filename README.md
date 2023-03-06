6fc47f18b267c717cc265e36f6909d5472144932
alexnesov <alexandrenesovic@gmail.com>
2023-03-06 10:28:44 +0100

    comp restruct

--- README.md
+++ README.md
@@ -17,4 +17,5 @@
 More advanced interesting example: https://threejs.org/examples/#webgl_camera <br>
 
 
+
 Let's go further: https://vercel.com/blog/building-an-interactive-webgl-experience-in-next-js
--- react-three-fiber-boilerplate/src/index.jsx
+++ react-three-fiber-boilerplate/src/index.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import './styles.css'
+import{ Box } from './Box'
+ 
+
+function App() {
+  return (
+  <Canvas camera={{ position: [0, 0, 2] }}>
+      <Box />
+  </Canvas>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color={0x00ff00} wireframe />
-      </mesh>
-    </Canvas>
+      <App />
   </StrictMode>
 )

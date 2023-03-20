e22db27b4b2fc2791fa546a74610783b2dabd193
alexnesov <alexandrenesovic@gmail.com>
2023-03-20 11:41:41 +0100

    OrbitControls added

--- react-three-fiber-boilerplate/src/App.jsx
+++ react-three-fiber-boilerplate/src/App.jsx
@@ -1,15 +1,23 @@
-import { Canvas } from '@react-three/fiber';
-import Box from './Box';
-
-
+import { Canvas } from '@react-three/fiber'
+import Polyhedron from './Polyhedron'
+import * as THREE from 'three'
+import { Stats, OrbitControls}from '@react-three/drei'
 
 export default function App() {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398),
+  ]
+
   return (
-    <Canvas camera={{ position: [0, 0, 3], rotation: [0, 0, 0] }}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-0.75, 0, 0]} name="A" />
-      <Box position={[0.75, 0, 0]} name="B" />
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <OrbitControls enableDamping={true}/>
+      <Stats />
     </Canvas>
-  );
+  )
 }
---
+++ react-three-fiber-boilerplate/src/Polyhedron.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export default function Polyhedron({ position, polyhedron }) {
+  const ref = useRef()
+  const [count, setCount] = useState(0)
+
+  console.log(polyhedron)
+
+  useFrame((_, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += 0.5 * delta
+  })
+
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerDown={() => {
+
+        let n = (count + 1) % 3
+        console.log(n)
+        setCount(n)
+      }}
+      geometry={polyhedron[count]}
+    >
+      <meshBasicMaterial color={'lime'} wireframe />
+    </mesh>
+  )
+}
